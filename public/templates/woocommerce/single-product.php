653b3a11fb03396d2ccb3a40fd700665f675cc2f
MubinMansuri <mubinmansuri41@gmail.com>
2021-09-17 15:37:36 +0530

    Add Ship Gallery Section in Detail page...

--- public/templates/woocommerce/single-product.php
+++ public/templates/woocommerce/single-product.php
@@ -144,9 +144,9 @@ $banner_image_url = ( ! empty( $banner_image_url ) ) ? $banner_image_url : ERSRV
 							</div>
 						</div>
 					</div>
-					<div class="ship-gallery info-box">
+					<!-- <div class="ship-gallery info-box">
 						<a class="section-title font-Poppins font-size-24 font-weight-bold d-block color-black text-decoration-none" data-toggle="collapse" href="#ship-gallery-collapse" role="button" aria-expanded="true" aria-controls="ship-gallery-collapse">
-							<span class=""><?php esc_html_e( 'Ship Gallery', 'easy-reservations' ); ?></span>
+							<span class=""><?php //esc_html_e( 'Ship Gallery', 'easy-reservations' ); ?></span>
 						</a>
 						<div class="collapse show" id="ship-gallery-collapse">
 							<div class="dropdown-divider"></div>
@@ -154,7 +154,6 @@ $banner_image_url = ( ! empty( $banner_image_url ) ) ? $banner_image_url : ERSRV
 								<div class="product-preview-main">
 									<img src="<?php echo esc_url( $featured_image_src ); ?>" alt="featured-image" class="product-preview-image" /> 
 								</div>
-								<!-- GALLERY IMAGES -->
 								<div id="preview-list" class="product-preview-menu mw-100">
 									<?php if ( ! empty( $gallery_image_ids ) && is_array( $gallery_image_ids ) ) { ?>
 										<?php foreach ( $gallery_image_ids as $image_id ) {
@@ -168,7 +167,7 @@ $banner_image_url = ( ! empty( $banner_image_url ) ) ? $banner_image_url : ERSRV
 								</div>
 							</div>
 						</div>
-					</div>
+					</div> -->
 					<div class="ship-description info-box">
 						<a class="section-title font-Poppins font-size-24 font-weight-bold d-block color-black text-decoration-none" data-toggle="collapse" href="#ship-description-collapse" role="button" aria-expanded="true" aria-controls="ship-description-collapse">
 							<span class=""><?php esc_html_e( 'Ship Description', 'easy-reservations' ); ?></span>
@@ -176,6 +175,27 @@ $banner_image_url = ( ! empty( $banner_image_url ) ) ? $banner_image_url : ERSRV
 						<div class="collapse show" id="ship-description-collapse">
 							<div class="dropdown-divider"></div>
 							<?php echo wp_kses_post( $item_post->post_content ); ?>
+							<div class="masonry-grid gallery-images">
+								<!--Item -->
+								<img src="https://source.unsplash.com/random?var-0" alt="" class="masonry-grid__item gallery-image-item" />
+								<img src="https://source.unsplash.com/random?var-1" alt="" class="masonry-grid__item gallery-image-item" />
+								<img src="https://source.unsplash.com/random?var-2" alt="" class="masonry-grid__item gallery-image-item" />
+								<img src="https://source.unsplash.com/random?var-3" alt="" class="masonry-grid__item gallery-image-item" />
+								<img src="https://source.unsplash.com/random?var-4" alt="" class="masonry-grid__item gallery-image-item" />
+								<img src="https://source.unsplash.com/random?ver-5" alt="" class="masonry-grid__item gallery-image-item" />
+								<!-- <div >
+								</div>
+								<div class="masonry-grid__item gallery-image-item">
+								</div>
+								<div class="masonry-grid__item gallery-image-item">
+								</div>
+								<div class="masonry-grid__item gallery-image-item">
+								</div>
+								<div class="masonry-grid__item gallery-image-item">
+								</div>
+								<div class="masonry-grid__item gallery-image-item">
+								</div> -->
+							</div>
 						</div>
 					</div>
 					<div class="price-details info-box">
@@ -396,6 +416,82 @@ $banner_image_url = ( ! empty( $banner_image_url ) ) ? $banner_image_url : ERSRV
 			</div>
 		</div>
 	</div>
+	<!-- light box HTML -->
+	<div class="lightbox">
+		<div class="title"></div>
+		<div class="filter"></div>
+		<div class="arrowr"></div>
+		<div class="arrowl"></div>
+		<div class="close"></div>
+	</div>
+	<script>
+		jQuery(window).load(function($) {
+
+			jQuery(".masonry-grid img").click(function() {
+				jQuery(".lightbox").fadeIn(300);
+				jQuery(".lightbox").append("<img src='" + jQuery(this).attr("src") + "' alt='" + jQuery(this).attr("alt") + "' />");
+				jQuery(".filter").css("background-image", "url(" + jQuery(this).attr("src") + ")");
+				/*jQuery(".title").append("<h1>" + jQuery(this).attr("alt") + "</h1>");*/
+				jQuery("html").css("overflow", "hidden");
+				if (jQuery(this).is(":last-child")) {
+					jQuery(".arrowr").css("display", "none");
+					jQuery(".arrowl").css("display", "block");
+				} else if (jQuery(this).is(":first-child")) {
+					jQuery(".arrowr").css("display", "block");
+					jQuery(".arrowl").css("display", "none");
+				} else {
+					jQuery(".arrowr").css("display", "block");
+					jQuery(".arrowl").css("display", "block");
+				}
+			});
+
+			jQuery(".close").click(function() {
+				jQuery(".lightbox").fadeOut(300);
+				jQuery("h1").remove();
+				jQuery(".lightbox img").remove();
+				jQuery("html").css("overflow", "auto");
+			});
+
+			jQuery(document).keyup(function(e) {
+				if (e.keyCode == 27) {
+					jQuery(".lightbox").fadeOut(300);
+					jQuery(".lightbox img").remove();
+					jQuery("html").css("overflow", "auto");
+				}
+			});
+
+			jQuery(".arrowr").click(function() {
+				var imgSrc = jQuery(".lightbox img").attr("src");
+				var search = jQuery(".masonry-grid").find("img[src$='" + imgSrc + "']");
+				var newImage = search.next().attr("src");
+				/*jQuery(".lightbox img").attr("src", search.next());*/
+				jQuery(".lightbox img").attr("src", newImage);
+				jQuery(".filter").css("background-image", "url(" + newImage + ")");
+
+				if (!search.next().is(":last-child")) {
+					jQuery(".arrowl").css("display", "block");
+				} else {
+					jQuery(".arrowr").css("display", "none");
+				}
+			});
+
+			jQuery(".arrowl").click(function() {
+				var imgSrc = jQuery(".lightbox img").attr("src");
+				var search = jQuery(".masonry-grid").find("img[src$='" + imgSrc + "']");
+				var newImage = search.prev().attr("src");
+				/*jQuery(".lightbox img").attr("src", search.next());*/
+				jQuery(".lightbox img").attr("src", newImage);
+				jQuery(".filter").css("background-image", "url(" + newImage + ")");
+
+				if (!search.prev().is(":first-child")) {
+					jQuery(".arrowr").css("display", "block");
+				} else {
+					jQuery(".arrowl").css("display", "none");
+				}
+			});
+
+		});
+	</script>
 </section>
 <?php
 get_footer();
